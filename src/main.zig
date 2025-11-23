7d0604b2fc84d76349e78d763af855314d85551d
Jan200101 <sentrycraft123@gmail.com>
2025-11-23 21:21:49 +0100

    add prints to test program to make it clearer what it does

--- src/main.zig
+++ src/main.zig
@@ -11,12 +11,20 @@ const LOGGERDB_OK = loggerdb.LOGGERDB_OK;
 pub fn main() !void {
     std.debug.print("LOGGERDB_OK = {}\n", .{LOGGERDB_OK});
 
+    std.debug.print("opening database\n", .{});
     var db: loggerdb.loggerdb = undefined;
-    defer assert(loggerdb.ldb_close(&db) == LOGGERDB_OK);
+    defer {
+        std.debug.print("closing database\n", .{});
+        assert(loggerdb.ldb_close(&db) == LOGGERDB_OK);
+    }
     assert(loggerdb.ldb_open(DB_NAME, &db) == LOGGERDB_OK);
 
+    std.debug.print("opening table\n", .{});
     var table: loggerdb.loggerdb_table = undefined;
-    defer assert(loggerdb.ldb_table_close(&table) == LOGGERDB_OK);
+    defer {
+        std.debug.print("closing table\n", .{});
+        assert(loggerdb.ldb_table_close(&table) == LOGGERDB_OK);
+    }
     assert(loggerdb.ldb_table_open(&db, TABLE_NAME, &table) == LOGGERDB_OK);
     assert(loggerdb.ldb_table_valid(&table) == LOGGERDB_OK);
 }
